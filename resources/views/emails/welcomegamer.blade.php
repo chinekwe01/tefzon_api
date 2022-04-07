713b5a1369f2f1839cce84489c36a47c11ecf053
arudovwen <successahon@gmail.com>
2022-04-07 23:36:22 +0100

    up

--- resources/views/emails/welcomegamer.blade.php
+++ resources/views/emails/welcomegamer.blade.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome Gamer</title>
 </head>
+
 <body>
-  Welcome Gamer
+    <h1>Welcome Gamer</h1>
+    <h2>Hi {{ $username }}</h2>
+    <p>Welcome onboard, pleased to meet you.</p>
+    <p>Thanks for joining TEFZON Fantasy community. You’re just one click away!</p>
+    <p style="margin-bottom:2rem">Kindly tap the button to get your email address verified and you're all set for a
+        great gaming
+        experience.</p>
+    <p>
+        <strong>TEFZON Fantasy Team.</strong>
+    </p>
 </body>
+
 </html>
