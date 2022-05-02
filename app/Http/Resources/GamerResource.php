3608096c125ad7bd57f6602ee6b775bfec4b3ef3
arudovwen <successahon@gmail.com>
2022-05-02 22:42:41 +0100

    Update GamerResource.php

--- app/Http/Resources/GamerResource.php
+++ app/Http/Resources/GamerResource.php
@@ -14,6 +14,6 @@ class GamerResource extends JsonResource
      */
     public function toArray($request)
     {
-        return ['id' => $this->id, 'name' => $this->name, 'username' => $this->username, 'avatar' => $this->avatar, 'email' => $this->email, 'referral_link' => $this->referral_link, 'phone' => $this->phone];
+        return ['id' => $this->id, 'first_name' => $this->first_name, 'last_name' => $this->last_name, 'username' => $this->username, 'avatar' => $this->avatar, 'email' => $this->email, 'referral_link' => $this->referral_link, 'phone' => $this->phone, 'gender' => $this->gender, 'dob' => $this->dob, 'country' => $this->country];
     }
 }
